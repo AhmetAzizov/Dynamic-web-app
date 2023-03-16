9f007a6fa9d344a775cac13865099c5feee4eaf8
AhmetAzizov <ahmet.azizov@gmail.com>
2023-03-16 02:03:03 +0100

    bug fix in registration form

--- index.js
+++ index.js
@@ -4,22 +4,39 @@ let dbRef = db.collection("users");
 
 
 function registerButton() {
-    let name = document.getElementById("name").value;
-    let details = document.getElementById("details").value;
-    let time = document.getElementById("time").value;
+    // let name = document.getElementById("name").value;
+    // let details = document.getElementById("details").value;
+    // let time = document.getElementById("time").value;
 
-    // if(name == "" || details == "" || time == "") return;
-  
+    const name = document.getElementsByName("name")[0].value;
+    const details = document.getElementsByName("details")[0].value;
+    const time = document.getElementsByName("time")[0].value;
+
+    // name.setAttribute('value', name.value);
+    // details.setAttribute('value', details.value);
+    // time.setAttribute('value', time.value);
+
+
+    if(name == "" || details == "" || time == "") return;
+
+    alert(name + " " + details + " " + time)
+    console.log(firebase)
+
+
     dbRef
     .add({
         Name: name,
         Details: details,
         Time: time
     })
-    // .then((docRef) => {
-    //     alert("Registration made with ID: ", docRef.id);
-    // })
-    // .catch((error) => {
-    //     alert("Error adding document: ", error);
-    // });
+    .then((docRef) => {
+        alert("Registration made with ID: ", docRef.id);
+        window.location.href = "list.html";
+    })
+    .catch((error) => {
+        alert("Error adding document: ", error);
+    });
+
   }
+
+
